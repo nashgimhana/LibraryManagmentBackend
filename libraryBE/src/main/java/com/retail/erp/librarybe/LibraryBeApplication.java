d1fdc402ef5f179fb53eac1aef025e5bf0653c42
nashgimhana <nadeesharb@gmail.com>
2025-09-28 10:38:15 +0800

    Project commit

---
+++ libraryBE/src/main/java/com/retail/erp/librarybe/LibraryBeApplication.java
@@ -0,0 +1,13 @@
+package com.retail.erp.librarybe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryBeApplication.class, args);
+    }
+
+}
---
+++ libraryBE/src/test/java/com/retail/erp/librarybe/LibraryBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retail.erp.librarybe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
